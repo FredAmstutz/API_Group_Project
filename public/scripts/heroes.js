a5eef83ba3b3967b046fe13a2f37617e52d23e53
unknown <nguyenduy1918@gmail.com>
2018-02-12 21:21:39 -0500

    added new functions

--- public/scripts/heroes.js
+++ public/scripts/heroes.js
@@ -1,18 +1,39 @@
 $(function(){
     $('.hero').on('click', 'button', getHeroImage)
+
 });
     
  function getHeroImage () {
-    const $this = $(this);
-    let $name = $this.parent().children('input');
+    console.log('clicked');
+     const $this = $(this);
+    //console.log($this.data);
+    //$(this).parent().remove();
+    const $name = $this.parent().children('input').val();
+    if($name == ""){
+        alert('You need to type the name of your Hero.');
+        return;
+    }
+    console.log($name);
     const result = $.ajax({
     url: `/name/${$name.val()}`
     })
     result.then(function(data) {
+        console.log(data);
+        if(data == '404'){
+            alert('Your name is wrong.');
+            return false;
+        }
+        const $id = data.heroId;
         let imageUrl = data.thumbnailJpg;
         imageUrl += '/portrait_xlarge.jpg';
         let $heroImage = $this.parent().children('.thumbnail')
+        $heroImage.attr('id', `${$id}`);
         $heroImage.attr('style',`background-image: url("${imageUrl}")`)
-    });
-    $name.val('');
-};
+        $('.id').on('click', function(){
+            // const heroImage = $('.id');
+            // let heroDetail = data.heroUrl;
+            console.log('clicked');
+
+        })
+        });
+    };
