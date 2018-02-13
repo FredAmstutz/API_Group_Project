575d4c8b0721b1a101f7b3bdb4f5d36e4a054642
FredAmstutz <fred4892@gmail.com>
2018-02-12 19:19:14 -0500

    Preparing to rebase with Duy code

--- public/scripts/cities.js
+++ public/scripts/cities.js
@@ -1,36 +1,33 @@
+const cityMenu = document.getElementById('city-menu');
+const cityButton = document.getElementById('city-button');
 
-    const cityMenu = document.getElementById('city-menu');
-    const cityButton = document.getElementById('city-button');
+cityButton.addEventListener('click', function() {
+    var background = document.querySelector('body');
+    
+    if(cityMenu.value === 'losangeles') {
+        background.classList = '';
+        background.classList.add('losangeles');
+    } else if(cityMenu.value === 'london') {
+        background.classList = '';
+        background.classList.add('london');
+    } else if(cityMenu.value === 'paris') {
+        background.classList = '';
+        background.classList.add('paris');
+    } else if(cityMenu.value === 'tokyo') {
+        background.classList = '';
+        background.classList.add('tokyo');
+    } else if(cityMenu.value === 'sydney') {
+        background.classList = '';
+        background.classList.add('sydney');
+    } else if(cityMenu.value === 'dubai') {
+        background.classList = '';
+        background.classList.add('dubai');
+    } else if(cityMenu.value === 'rio') {
+        background.classList = '';
+        background.classList.add('rio');
+    } else if(cityMenu.value === 'newyork') {
+        background.classList = '';
+        background.classList.add('newyork');
+    }
+});
 
-    cityButton.addEventListener('click', function() {
-        var background = document.querySelector('body');
-        
-        if(cityMenu.value === 'losangeles') {
-            background.classList = '';
-            background.classList.add('losangeles');
-        } else if(cityMenu.value === 'london') {
-            background.classList = '';
-            background.classList.add('london');
-        } else if(cityMenu.value === 'paris') {
-            background.classList = '';
-            background.classList.add('paris');
-        } else if(cityMenu.value === 'tokyo') {
-            background.classList = '';
-            background.classList.add('tokyo');
-        } else if(cityMenu.value === 'sydney') {
-            background.classList = '';
-            background.classList.add('sydney');
-        } else if(cityMenu.value === 'dubai') {
-            background.classList = '';
-            background.classList.add('dubai');
-        } else if(cityMenu.value === 'rio') {
-            background.classList = '';
-            background.classList.add('rio');
-        } else if(cityMenu.value === 'newyork') {
-            background.classList = '';
-            background.classList.add('newyork');
-        }
-    });
-
-
-  
